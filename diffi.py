fe26778d932235b8e148593cff452e24e51f2bbc
Adam Domagalski <adam.domagalski@recoded.co>
2018-11-14 08:49:34 +0100

    change into exponenstial pow

--- diffi.py
+++ diffi.py
@@ -4,7 +4,7 @@ from Crypto.Util import number
 # teraz to wrzucamy do 3des
 # https://en.wikipedia.org/wiki/Diffie%E2%80%93Hellman_key_exchange
 
-INIT_PRIME_SIZE = 200 # secound prime number will be twice +1 bigger
+INIT_PRIME_SIZE = 512 # secound prime number will be twice +1 bigger
 
 def getPrimeRoot():
   while True:
@@ -15,31 +15,23 @@ def getPrimeRoot():
 
   while True:
     g = randint(2,p-1)
-    if (g**2 != 1%p) and (g**q != 1%p):
+    if (pow(g,2) != 1%p) and (pow(g,q,p) != 1%p):
       return g, p
 
-
 g, p = getPrimeRoot()
-
 alicePrivateKey = randint(1,p-1)
 bobPrivateKey   = randint(1,p-1)
 print('Alice private key: %d' % alicePrivateKey)
-print('Bob private key:   %d' % bobPrivateKey)
+print('  Bob private key: %d' % bobPrivateKey)
 
-alicePublicKey  = (g**alicePrivateKey)  % p
-bobPublicKey    = (g**bobPrivateKey)    % p
-print('Alice public key:  %d' % alicePublicKey)
-print('Bob public key:    %d' % bobPublicKey)
-
-
-alice = (bobPublicKey**alicePrivateKey) % p
-bob = (alicePublicKey**bobPrivateKey)   % p
-print('Alice key: %d' % alice)
-print('Bob key:   %d' % bob)
-
-print(alice == bob)
+alicePublicKey  = pow(g,alicePrivateKey,p)
+bobPublicKey    = pow(g,bobPrivateKey,p)
+print(' Alice public key: %d' % alicePublicKey)
+print('   Bob public key: %d' % bobPublicKey)
 
 
-
-
-
+alice = pow(bobPublicKey,alicePrivateKey,p)
+bob = pow(alicePublicKey,bobPrivateKey,p)
+print('        Alice key: %d' % alice)
+print('          Bob key: %d' % bob)
+print(' Same secret?\n\tA==B: %s' % (alice == bob))
